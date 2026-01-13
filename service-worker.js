301c38f36dd5c9662b0b92ee22b1021cbf71a7f5
easyadpromotion <68439771+easyadpromotion@users.noreply.github.com>
2026-01-13 12:47:38 +0530

    Refactor service worker to use cache versioning

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,39 @@
+const CACHE_NAME = "eap-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("eap-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
+// Activate (cleanup old caches)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
